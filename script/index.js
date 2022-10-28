fa069a8e6d005123feb80c362deccd5e4a720723
Andrey210199 <dtyaew@mail.ru>
2022-10-28 23:35:05 +0300

    id_fixed

--- script/index.js
+++ script/index.js
@@ -1,6 +1,5 @@
 //Импорт файлов
 import * as Popup from "./popup/popup.js";
-import * as AddCat from "./popup/addCat.js";
 import * as Constant from "./constant.js";
 import * as UpdatePopup from "./popup/popup-update.js";
 import { PopupAddCat } from "./popup/popup-add.js";
@@ -18,7 +17,6 @@ const popupUpdateCat = new UpdatePopup.UpdatePopup("popup-update-cat", Constant.
 
 const loginBtn = document.querySelector(".header__login");
 const addCatBtn = document.querySelector(".header__btn");
-const popupAddCat = document.querySelector("#form__add-cat");
 const cards = document.querySelector(".cards");
 
 const loginForm = document.querySelector("#form__login");
@@ -32,6 +30,11 @@ const link = "card__name";
 
 
 //Вызовы функций
+function updatePopupOpen(data, updateId){
+    UpdatePopup.showData(data,updateId);
+    popupUpdateCat.openPopup();
+    popupUpdateCat.blur();
+}
 loginBtn.addEventListener("click", (evt)=>{
     if(isAuth)
     {   document.cookie= `email= ${getCookie().email}; max-age=-1`;
@@ -43,7 +46,15 @@ loginBtn.addEventListener("click", (evt)=>{
     }
     
 });
-addCatBtn.addEventListener("click", (evt)=>{addcat.openPopup()});
+
+addCatBtn.addEventListener("click", (evt)=>{
+    api.getAllId()
+    .then(({data})=>{
+       const id = data[data.length-1]+1;
+        addcat.openPopup(id);
+    });
+
+});
 
 loginForm.addEventListener("submit",(event)=>{
      event.preventDefault();
@@ -70,13 +81,25 @@ cards.addEventListener("click", (evt)=>{
 
     if(evt.target.className === link)
     {
+        let localBoolean =false;
         const id =evt.target.closest("[id]").id;
-        api.getAllCAtsOrCatById(id)
-        .then(({data})=>{
-            UpdatePopup.showData(data,updateId);
-            popupUpdateCat.openPopup();
-            popupUpdateCat.blur();
-        })
+        const localS = JSON.parse(localStorage.getItem("cats"));
+        localS.forEach((elem)=>{
+            
+            if(elem.id === Number(id))
+            {
+               localBoolean =true;
+               updatePopupOpen(elem, updateId);  
+            }
+        });
+
+        if(!localBoolean)
+        {
+            api.getAllCAtsOrCatById(id)
+            .then(({data})=>{
+                updatePopupOpen(data, updateId)
+            })
+        }
 
     }
 });
@@ -85,7 +108,3 @@ cards.addEventListener("click", (evt)=>{
 checkLStor();
 Authorized(loginBtn);
 
-/* api.getAllId()
-.then(({data})=>{
-    console.log(data[data.length-1]+1);
-}) */
--- script/popup/popup-add.js
+++ script/popup/popup-add.js
@@ -10,11 +10,11 @@ export class PopupAddCat extends Popup{
         super(selector, photo);
         this._api =  new Api(Constant.CONFIG_API);;
         this._formId = this._popup.querySelector(`#${formId}`);
+        this._formArr = [...this._formId.elements];
     }
 
      _add=(event)=>{
         event.preventDefault();
-         this._formArr = [...this._formId.elements];
          this._dataFromForm =AddCat.addCat(this._formArr,Constant.NOPHOTO);
 
         this._api.addCat(this._dataFromForm)
@@ -26,8 +26,21 @@ export class PopupAddCat extends Popup{
         });
     }
 
-    openPopup(){
+    _popupId=()=>{
+        this._formArr.forEach(element => {
+            
+            if(element.name ==="id")
+            {
+                element.value = this._id;
+            }
+        });
+        
+    }
+
+    openPopup(id){
         super.openPopup();
+        this._id = id;
+        this._popupId();
         this._formId.addEventListener("submit", this._add);
     }
 
